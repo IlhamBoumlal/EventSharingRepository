aa449708330689245ae38e100d5a83452cfc0fb9
Ilham Boumlal <boumlalilham@gmail.com>
2026-04-09 11:58:59 +0100

    first commit

---
+++ EventSharing/EventSharing/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventSharing.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
